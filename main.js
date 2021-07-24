376b16b800d1892df9b7b0dd9b88261b8ba79557
PERSIANencrypter <79386552+PERSIANencrypter@users.noreply.github.com>
2021-07-24 05:48:02 +0430

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function yek(){
+  document.getElementById('inp2').value+="1"
+}
+function mytwo(){
+  document.getElementById('inp2').value+="2"
+}
+function se(){
+  document.getElementById('inp2').value+="3"
+}
+function char(){
+  document.getElementById('inp2').value+="4"
+}
+function panj(){
+  document.getElementById('inp2').value+="5"
+}
+function shish(){
+  document.getElementById('inp2').value+="6"
+}
+function haft(){
+  document.getElementById('inp2').value+="7"
+}
+function hasht(){
+  document.getElementById('inp2').value+="8";
+}
+function noh(){
+  document.getElementById('inp2').value+="9"
+}
+function sefr(){
+  document.getElementById('inp2').value+="0"
+}
+function jam(){
+  document.getElementById('inp2').value+="+"
+}
+function menha(){
+  document.getElementById('inp2').value+="-"
+}
+function zarb(){
+  document.getElementById('inp2').value+="*"
+}
+function taghsim(){
+  document.getElementById('inp2').value+="/"
+}
+function c(){
+  document.getElementById('inp2').value=""
+}
+function hesab(){
+  document.getElementById('inp2').value=eval(document.getElementById('inp2').value)
+}
